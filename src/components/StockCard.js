939481f00edc1d6978a6911d7c1b71a8c7d279c7
Uduak Essien <acushla4real@gmail.com>
2020-12-05 22:38:36 +0100

    Add react-icons and image proptypes

--- src/components/StockCard.js
+++ src/components/StockCard.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { Card, Button } from 'react-bootstrap';
 import PropTypes from 'prop-types';
+import { BsGraphDown, BsGraphUp } from 'react-icons/bs';
 
 function StockCard({ stock }) {
   return (
     <Card style={{ width: '14rem' }} className="m-2 bg-dark">
-      {/* <Card.Img variant="top" src="https://financialmodelingprep.com/image-stock/AAPL.png" /> */}
+      <Card.Img variant="top" src={stock.image}/>
       <Card.Body className="d-flex flex-column">
         <Card.Title className="text-muted font-weight-bold">{ stock.symbol }</Card.Title>
         <Card.Subtitle className="mb-2 text-muted">{ stock.company }</Card.Subtitle>
-        <Card.Text className="text-success">
-          { `${stock.price} ${stock.currency || 'USD'}` }
+        <Card.Text className={stock.percentage > 0 ? 'text-success' : 'text-danger'}>
+          { ` ${stock.price} ${stock.currency || 'USD'}` }
           {' '}
           <br />
-          <em>{ `${stock.percentage}%` }</em>
+          <em>{ `${stock.percentage}% ` }</em>
+          {stock.percentage > 0 ? <BsGraphUp /> : <BsGraphDown />}
         </Card.Text>
         <Button variant="outline-success mt-auto">Details</Button>
       </Card.Body>
@@ -28,6 +30,7 @@ StockCard.propTypes = {
     price: PropTypes.number.isRequired,
     currency: PropTypes.number,
     percentage: PropTypes.number.isRequired,
+    image: PropTypes.string.isRequired,
   }).isRequired,
 };
 
--- src/container/StockContainer.js
+++ src/container/StockContainer.js
@@ -7,6 +7,7 @@ export default function Stockcontainer() {
     price: 250.03,
     percentage: 0.55,
     company: 'Apple Inc',
+    image: 'https://financialmodelingprep.com/image-stock/AAPL.png',
   };
 
   const atlas = {
@@ -14,13 +15,22 @@ export default function Stockcontainer() {
     price: 56.370,
     percentage: 0.660,
     company: 'Atlas Air Worldwide Holdings, Inc.',
+    image: 'https://financialmodelingprep.com/image-stock/AAPL.png',
+  };
+
+  const pan = {
+    symbol: 'PAAS',
+    price: 30.58,
+    percentage: -0.110,
+    company: 'Pan American Silver Corp.',
+    image: 'https://financialmodelingprep.com/image-stock/AAPL.png',
   };
 
   return (
     <div className="mt-5 d-flex flex-wrap justify-content-center">
       <Stockcard stock={apple} />
       <Stockcard stock={atlas} />
-      <Stockcard stock={apple} />
+      <Stockcard stock={pan} />
       <Stockcard stock={atlas} />
     </div>
   );
