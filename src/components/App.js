9db587cadb9aa50a15ff48c51a82692bee69c822
Uduak Essien <acushla4real@gmail.com>
2020-12-05 13:03:51 +0100

    Update TopNav styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
+import { Container } from 'react-bootstrap';
 import TopNav from './TopNav';
 
 function App() {
   return (
-    <>
+    <Container className="pt-5 pb-5">
       <TopNav />
-    </>
+    </Container>
   );
 }
 
--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import {
-  Navbar, Nav, Form, FormControl, Button, Container,
+  Navbar,
+  Nav,
+  Form,
+  FormControl,
+  Button,
 } from 'react-bootstrap';
 
 export default function TopNav() {
   return (
     <Navbar bg="dark" variant="dark">
-      <Container>
-        <Navbar.Brand href="#home">STOCKIST</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">About</Nav.Link>
-          <Nav.Link href="#pricing">Stock News</Nav.Link>
-        </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search Stock" className="mr-sm-2" />
-          <Button variant="outline-info">Search</Button>
-        </Form>
-      </Container>
+      <Navbar.Brand href="#home">STOCKIST</Navbar.Brand>
+      <Nav className="mr-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#features">About</Nav.Link>
+        <Nav.Link href="#pricing">Stock News</Nav.Link>
+      </Nav>
+      <Form inline>
+        <FormControl
+          type="text"
+          placeholder="Search Stock"
+          className="mr-sm-2"
+        />
+        <Button variant="outline-info">Search</Button>
+      </Form>
     </Navbar>
   );
 }
