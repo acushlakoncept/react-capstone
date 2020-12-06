8b55e0da04a66070f833810e1335f079c13da325
Uduak Essien <acushla4real@gmail.com>
2020-12-06 12:44:44 +0100

    Refactor StockDetails to accept props and add propTypes validation

--- src/components/StockDetail.js
+++ src/components/StockDetail.js
@@ -1,35 +1,42 @@
-/* eslint-disable max-len */
 import React from 'react';
 import { Jumbotron, Container } from 'react-bootstrap';
+import PropTypes from 'prop-types';
 import { BsFillForwardFill } from 'react-icons/bs';
 import { FcBullish } from 'react-icons/fc';
 
-export default function Stockdetail() {
+export default function StockDetail({ stockItem }) {
   return (
     <div>
       <Jumbotron fluid>
         <Container>
           <h1 className="shadow-sm pb-2 mb-3">
-            AAPL
+            { stockItem.symbol }
             <small className="priceChange">
               <BsFillForwardFill color="transparent" />
-              Price: 122.25
+              Price:
+              {' '}
+              { stockItem.price }
               <BsFillForwardFill color="transparent" />
-              Price change: -0.69
+              Price change:
+              {' '}
+              { stockItem.changes }
               <BsFillForwardFill color="transparent" />
               <FcBullish size={40} />
             </small>
           </h1>
           <h4 className="text-center text-success">
-            Apple Inc
+            { stockItem.companyName }
             <BsFillForwardFill color="transparent" />
-            <small>Industry: Consumer Electronics</small>
+            <small>
+              Industry:
+              { stockItem.industry }
+            </small>
             <hr />
           </h4>
           <p className="lead text-justify readMaxLength">
-            <img src="https://financialmodelingprep.com/image-stock/AAPL.png" alt="apple" className="float-left m-2" />
-            Apple Inc. designs, manufactures, and markets smartphones, personal computers, tablets, wearables, and accessories worldwide. It also sells various related services. The company offers iPhone, a line of smartphones; Mac, a line of personal computers; iPad, a line of multi-purpose tablets; and wearables, home, and accessories comprising AirPods, Apple TV, Apple Watch, Beats products, HomePod, iPod touch, and other Apple-branded and third-party accessories. It also provides AppleCare support services; cloud services store services; and operates various platforms, including the App Store, that allow customers to discover and download applications and digital content, such as books, music, video, games, and podcasts. In addition, the company offers various services, such as Apple Arcade, a game subscription service; Apple Music, which offers users a curated listening experience with on-demand radio stations; Apple News+, a subscription news and magazine service; Apple TV+, which offers exclusive original content; Apple Card, a co-branded credit card; and Apple Pay, a cashless payment service, as well as licenses its intellectual property. The company serves consumers, and small and mid-sized businesses; and the education, enterprise, and government markets. It sells and delivers third-party applications for its products through the App Store. The company also sells its products through its retail and online stores, and direct sales force; and third-party cellular network carriers, wholesalers, retailers, and resellers. Apple Inc. was founded in 1977 and is headquartered in Cupertino, California.
-            <a href="https://www.apple.com/" target="__blank" rel="noopener">
+            <img src={stockItem.image} alt={stockItem.symbol} className="float-left m-2" />
+            { stockItem.description }
+            <a href={stockItem.website} target="__blank" rel="noopener">
               {' '}
               Visit Website
             </a>
@@ -39,3 +46,20 @@ export default function Stockdetail() {
     </div>
   );
 }
+
+StockDetail.propTypes = {
+  stockItem: PropTypes.shape({
+    symbol: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    changes: PropTypes.number.isRequired,
+    companyName: PropTypes.string.isRequired,
+    industry: PropTypes.string.isRequired,
+    website: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+  }),
+};
+
+StockDetail.defaultProps = {
+  stockItem: {},
+};
--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-// import App from './pages/App';
+import App from './pages/App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import store from './redux/store';
-import Detailpage from './pages/DetailPage';
+// import Detailpage from './pages/DetailPage';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <App /> */}
-      <Detailpage />
+      <App />
+      {/* <Detailpage /> */}
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
