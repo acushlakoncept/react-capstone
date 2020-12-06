5fb4e43546f76b7ad341b351558a96b24ba44946
Uduak Essien <acushla4real@gmail.com>
2020-12-06 07:57:02 +0100

    Clean up stock card

--- src/components/StockCard.js
+++ src/components/StockCard.js
@@ -4,13 +4,7 @@ import PropTypes from 'prop-types';
 import { BsGraphDown, BsGraphUp, BsFillForwardFill } from 'react-icons/bs';
 import notFound from '../imgs/not_found.jpg';
 
-// const onError = () => {
-//   if ('https://financialmodelingprep.com/image-stock/XRT.png') return notFound;
-//   return null;
-// };
 function StockCard({ stock }) {
-//   console.log(imageExists('https://financialmodelingprep.com/image-stock/XPEV.png'));
-// <img src="http://example.com/non-existent-image.jpg" onerror="this.onerror=null;this.src='http://example.com/existent-image.jpg';" />
   const justPercent = stock.changesPercentage.match(/(-|\+)|(\.)|\d+/g).join('');
   const percentage = parseFloat(justPercent);
   return (
@@ -34,25 +28,12 @@ function StockCard({ stock }) {
           {percentage > 0 ? <BsGraphUp /> : <BsGraphDown />}
         </Card.Text>
         <div className="mt-auto">
-          {/* <ul className="d-flex flex-wrap justify-content-between dayPrice text-muted">
-            <li>
-              <strong>24h Low</strong>
-              <br />
-              {stock.dayLow}
-            </li>
-            <li>
-              <strong>24h High</strong>
-              <br />
-              {stock.dayHigh}
-            </li>
-          </ul> */}
           <Button variant="outline-success w-100">Details</Button>
         </div>
       </Card.Body>
     </Card>
   );
 }
-// convert timestamp to date time (new Date(timestamp * 1000)).toLocaleString()
 
 StockCard.propTypes = {
   stock: PropTypes.shape({
@@ -61,8 +42,6 @@ StockCard.propTypes = {
     price: PropTypes.string.isRequired,
     currency: PropTypes.string,
     changesPercentage: PropTypes.string.isRequired,
-    // dayLow: PropTypes.number.isRequired,
-    // dayHigh: PropTypes.number.isRequired,
   }).isRequired,
 };
 
