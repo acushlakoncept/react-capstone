63e6df79a0a715bd3d18fd9698dd37aa09d78530
Uduak Essien <acushla4real@gmail.com>
2020-12-07 22:43:41 +0100

    Add handleSelect Prop to TopNav

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,11 +1,18 @@
+import { useDispatch } from 'react-redux';
 import { Container } from 'react-bootstrap';
 import TopNav from '../components/TopNav';
 import Stockcontainer from '../container/StockContainer';
+import { fetchStocks } from '../redux/index';
 
 function App() {
+  const dispatch = useDispatch();
+  const handleOnSelect = e => {
+    dispatch(fetchStocks(e.target.value));
+  };
+
   return (
     <Container className="pt-5 pb-5">
-      <TopNav />
+      <TopNav handleOnSelect={handleOnSelect} />
       <Stockcontainer />
     </Container>
   );
