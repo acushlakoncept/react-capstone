36b94e8b5e1f183c310b4f277d552437b031643d
Uduak Essien <acushla4real@gmail.com>
2020-12-08 16:29:04 +0100

    Add stock news container

--- src/container/StockNewsContainer.js
+++ src/container/StockNewsContainer.js
@@ -1,11 +1,29 @@
-import React from 'react';
+/* eslint-disable react/prop-types */
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
 import StockNews from '../components/StockNews';
+import { fetchNews } from '../redux';
 
-export default function StockNewsContainer() {
+function StockNewsContainer({ newsData, fetchNews }) {
+  useEffect(() => {
+    fetchNews();
+  }, []);
+
   return (
     <>
+      { console.log(newsData) }
       <StockNews />
       <StockNews />
     </>
   );
 }
+
+const mapStateToProps = state => ({
+  newsData: state.stockNews,
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetchNews: () => dispatch(fetchNews()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(StockNewsContainer);
