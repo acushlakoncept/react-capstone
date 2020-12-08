8da7e03dfb6748e1f0c704f733b55f6d03b64012
Uduak Essien <acushla4real@gmail.com>
2020-12-08 21:33:50 +0100

    Update StockNews Container to accept newsData asynchronously

--- src/container/StockNewsContainer.js
+++ src/container/StockNewsContainer.js
@@ -1,6 +1,8 @@
+/* eslint-disable no-nested-ternary */
 /* eslint-disable react/prop-types */
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import { ScaleLoader } from 'react-spinners';
 import StockNews from '../components/StockNews';
 import { fetchNews } from '../redux';
 
@@ -9,12 +11,21 @@ function StockNewsContainer({ newsData, fetchNews }) {
     fetchNews();
   }, []);
 
-  return (
-    <>
-      { console.log(newsData) }
-      <StockNews />
-      <StockNews />
-    </>
+  return newsData.loading ? (
+    <h2 className="text-center pt-5">
+      <ScaleLoader size={16} color="green" />
+    </h2>
+  ) : newsData.error || Object.keys(newsData.news) < 2 ? (
+    <h2 className="text-center pt-5">Kindly check back, Server currently not responding</h2>
+  ) : (
+    <div className="mt-5 d-flex flex-wrap justify-content-center">
+      {newsData.news.map(news => (
+        <StockNews
+          key={`${news.symbol}-${Math.floor(Math.random() * 11)}`}
+          news={news}
+        />
+      ))}
+    </div>
   );
 }
 
