7e25bddca648cbbe2f95d9425649bf9e1d2651da
Uduak Essien <acushla4real@gmail.com>
2020-12-08 11:16:21 +0100

    Implement handleFormClick on Detailpage

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -5,9 +5,14 @@ import TopNavTicker from '../components/TopNavTicker';
 import StockDetailsContainer from '../container/StockDetailsContainer';
 
 export default function Detailpage() {
+  const handleFormClick = e => {
+    e.preventDefault();
+    const url = `/${e.target[0].value}`;
+    window.location.href = url;
+  };
   return (
     <Container className="pt-5 pb-5">
-      <TopNavTicker />
+      <TopNavTicker handleFormClick={handleFormClick} />
       <StockDetailsContainer />
       <Footer />
     </Container>
