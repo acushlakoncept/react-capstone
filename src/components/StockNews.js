fb486285f9a19702f8798de9017f373e7d517f24
Uduak Essien <acushla4real@gmail.com>
2020-12-08 21:32:19 +0100

    Modify StockNews to accept props

--- src/components/StockNews.js
+++ src/components/StockNews.js
@@ -1,26 +1,31 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable max-len */
 import React from 'react';
 import {
   Jumbotron, Container, Row, Col,
 } from 'react-bootstrap';
 
-export default function StockNews() {
+export default function StockNews({ news }) {
   return (
     <Jumbotron fluid className="rounded mt-3 mb-2">
       <Container>
         <Row>
           <Col md={4}>
             {' '}
-            <img src="https://cdn.snapi.dev/images/v1/j/5/press14.jpg" className="img-fluid img-thumbnail rounded" alt="" />
+            <img src={news.image} className="img-fluid img-thumbnail rounded" alt="" />
           </Col>
           <Col md={8}>
-            <h1>QQQ</h1>
-            <h4>QQQ: Not A Tech ETF, Weirdly Constituted, But Very, Very Tempting</h4>
+            <h1>{news.symbol}</h1>
+            <h4>{news.title}</h4>
+            <small className="text-muted">
+              published:
+              {news.publishedDate}
+            </small>
+            <hr />
             <p className="lead">
-
-              QQQ is what many investors buy when they want to tilt towards Tech though it is not really a Tech ETF. It follows a delightfully irrational index very unlike most stodgy sector indexes, but its quirkiness is why it has delivered great return.
+              {news.text}
               <br />
-              <a href="https://seekingalpha.com/article/4393534-qqq-not-tech-etf-weirdly-constituted-tempting" className="text-light btn btn-success mt-2"> Read more</a>
+              <a href={news.url} className="text-light btn btn-success mt-2" target="__blank" rel="noopener"> Read more</a>
             </p>
           </Col>
         </Row>
