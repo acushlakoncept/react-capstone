337b7218443b9a4881ed32130499cffb613d1919
Uduak Essien <acushla4real@gmail.com>
2020-12-08 12:07:29 +0100

    Add .env file to gitignore and add stock news url

--- src/redux/stocks/stockUrls.js
+++ src/redux/stocks/stockUrls.js
@@ -6,6 +6,7 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 export const STOCK_LIST_URL = `https://financialmodelingprep.com/api/v3/actives?apikey=${API_KEY}`;
 export const STOCK_GAINERS = `https://financialmodelingprep.com/api/v3/gainers?apikey=${API_KEY}`;
 export const STOCK_LOSERS = `https://financialmodelingprep.com/api/v3/losers?apikey=${API_KEY}`;
+export const STOCK_NEWS = `https://financialmodelingprep.com/api/v3/stock_news?limit=50&apikey=${API_KEY}`;
 
 export const stockQuoteUrl = stockSymbol => (
   `https://financialmodelingprep.com/api/v3/quote/${stockSymbol}?apikey=${API_KEY}`
